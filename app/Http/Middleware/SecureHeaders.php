4ab2aeb6e78befed37945af8564998df9d9b2d07
harshshekhar1234 <ginyu@live.com>
2023-01-18 13:11:14 +0530

    first commit

--- app/Http/Middleware/SecureHeaders.php
+++ app/Http/Middleware/SecureHeaders.php
@@ -26,8 +26,8 @@ class SecureHeaders
         $response->headers->set('X-Content-Type-Options', 'nosniff');
         $response->headers->set('X-XSS-Protection', '1; mode=block');
         $response->headers->set('X-Frame-Options', 'DENY');
-        $response->headers->set('Strict-Transport-Security', 'max-age=31536000; includeSubDomains');
-        $response->headers->set('Content-Security-Policy', "style-src 'self' https://fonts.googleapis.com 'unsafe-inline'");
+        // $response->headers->set('Strict-Transport-Security', 'max-age=31536000; includeSubDomains');
+        // $response->headers->set('Content-Security-Policy', "style-src 'self' https://fonts.googleapis.com 'unsafe-inline'");
         return $response;
     }
     private function removeUnwantedHeaders($headerList)
